e28a937c1eefed4f45ff15d29e0061cb7c0cda6c
root <root@bd2f032115f3>
2021-10-13 01:31:43 +0000

    Test Scores

---
+++ src/_01_double/_2_test_scores/testscores.java
@@ -0,0 +1,14 @@
+package _01_double._2_test_scores;
+
+import javax.swing.JOptionPane;
+
+public class testscores {
+public static void main(String[] args) {
+String Input=	JOptionPane.showInputDialog(null,"Please enter your test score and make sure you have a decimal");
+double score=Double.parseDouble(Input);
+JOptionPane.showMessageDialog(null,"Wow man you aced that test ");
+if (80)
+	JOptionPane.showMessageDialog(null,"Wow man you bombed the test");
+}
+}
+//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
